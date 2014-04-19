d250e6ffc59a15cc046f92da683b0424cb722cf6
Emma <emma_williams1@hotmail.co.uk>
2014-04-19 14:33:21 +0100

    added land, release and full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,31 @@
 class Airport
+  #initialize the airport
+
 end
 
-class Plane
+def land plane 
 end
 
+def release plane
+end
+
+def full?
+end
+
+
+
+
+#request airplane to take off
+#no more planes can be added if airport is full
+#how many planes can land in the airport
+#if the airport is full no planes can land
+
+
+
+#a plane can land
+#a plane can take off
+#a plane cannot land if the airport is full
+
+
+#a plane cannot take off when there is a storm brewing
+#a plane cannot land in the middle of a storm
---
+++ lib/plane.rb
@@ -0,0 +1,16 @@
+class Plane
+end
+
+#when we create a new plane it should have a flying status
+
+#when landed planes status should change to landed
+
+#when it takes off the planes status should be changed to flying
+
+
+
+
+#flying staus when created
+#flying status when in the air
+#can take off
+#changes its status to flying after taking off
